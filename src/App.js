6b69befb8e29c5887efa6da395169e75fc4af2ff
EDWIN <84503319+Edwin254-byte@users.noreply.github.com>
2022-03-05 08:33:51 +0300

    first pre-version 1.0.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import 'tachyons';
+import Clarifai from 'clarifai';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from './components/Particles/Particles';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+      imageurl: ''
+    }
+  }
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+  onButtonSubmit = () => {
+      this.setState({imageurl: this.state.input})
+        const raw = JSON.stringify({
+         "user_app_id": {
+         "user_id": "edu254",
+         "app_id": "eduson"
+      },
+      "inputs": [
+        {
+          "data": {
+            "image": {
+              "url": this.state.input 
+            }
+          }
+        }
+      ]
+    });
+
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key 24dcbdf692534bf19636d658f620e5d4'
+      },
+      body: raw
+    };
+
+    // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
+    // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
+    // this will default to the latest version_id
+
+    fetch("https://api.clarifai.com/v2/models/face-detection/outputs", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(JSON.parse(result, null, 2).outputs[0].data.regions[0].region_info.bounding_box))
+      .catch(error => console.log('error', error));
+}
+    render(){
+      return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+        onInputChange={this.onInputChange} 
+        onButtonSubmit = {this.onButtonSubmit}/>
+        <Particles className='particles'/>
+        <FaceRecognition imageurl = {this.state.imageurl}/>
+     </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FaceRecognition = ({imageurl}) => {
+	return(
+		<div className = 'center'>
+			<div className='absolute mt2'>
+				<img alt = '' src={imageurl} width='500px' height='auto'/>
+			</div>
+		</div>
+	);
+}
+
+export default FaceRecognition;
